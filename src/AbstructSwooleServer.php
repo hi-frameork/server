f12ee568221fc7a0437032cc5751f373c93d0302
热心网友 <anoxia@engineer.com>
2021-03-03 23:05:30 +0800

    优化接口方法

--- src/AbstructSwooleServer.php
+++ src/AbstructSwooleServer.php
@@ -5,17 +5,5 @@ namespace Hi\Server;
 abstract class AbstructSwooleServer extends AbstructServer
 {
     abstract protected function createServer();
-
-    public function start(callable $handle, callable $taskHandle)
-    {
-    }
-
-    public function restart()
-    {
-    }
-
-    public function stop(bool $force = false)
-    {
-    }
 }
 
